e114e53b4ae972900c4e0e10bc31d68104bfee4b
Your Name <you@example.com>
2021-03-20 23:19:51 +0530

    change 1

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 from django.http import  HttpResponse
 def home(request):
     return render(request,'accounts/dashboard.html')
 def product(request):
-    return render(request, 'accounts/product.html')
+    products=Product.objects.all()
+    return render(request, 'accounts/product.html',{'products':products})
 def customer(request):
     return render(request, 'accounts/customer.html')
