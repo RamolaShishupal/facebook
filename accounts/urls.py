3c1e9655848b023887a35ef894a57564d0985f90
Your Name <you@example.com>
2021-03-21 22:37:46 +0530

    name

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='home'),
-    path('product/',views.product,name='login'),
-    path('customer/<str:pk_test>',views.customer,name='register'),
+    path('product/',views.product,name='product'),
+    path('customer/<str:pk_test>',views.customer,name='customer'),
 ]
