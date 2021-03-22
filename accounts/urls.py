38ed04bfe01a6700f83c8370000038164864464f
Your Name <you@example.com>
2021-03-22 16:15:11 +0530

    order form

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('product/',views.product,name='product'),
     path('customer/<str:pk_test>',views.customer,name='customer'),
+    path('create_order/',views.createOrder,name='create_order')
 ]
