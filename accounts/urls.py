9ffdf1a1cc89e5d1f9f627fbf05dc246a7272fc3
Your Name <you@example.com>
2021-03-22 16:41:01 +0530

    order form

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('product/',views.product,name='product'),
     path('customer/<str:pk_test>',views.customer,name='customer'),
-    path('create_order/',views.createOrder,name='create_order')
+    path('create_order/',views.createOrder,name='create_order'),
+    path('update_order/',views.updateOrder,name='update_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from .forms import OrderForm
 # Create your views here.
 from django.http import  HttpResponse
 def home(request):
@@ -27,5 +28,20 @@ def customer(request,pk_test):
     context={'customer':customer,'orders':orders,'order_count':order_count}
     return render(request, 'accounts/customer.html',context)
 def createOrder(request):
-    context={}
+    form=OrderForm()
+    if request.method=='POST':
+        print('Printing POST:',request.POST)
+
+
+        form =OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return  redirect('/')
+
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+def updateOrder(request):
+    form=OrderForm
+
+    context={'form':form}
     return render(request,'accounts/order_form.html',context)
