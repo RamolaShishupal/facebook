fe7549c7b141e4b9d3a8185c9b00872e10c08de0
Your Name <you@example.com>
2021-04-02 14:12:00 +0530

    3

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import *
 from .forms import OrderForm
+from django.forms import inlineformset_factory
+
 # Create your views here.
 from django.http import  HttpResponse
 def home(request):
