57390f9b5ef9f29d0a3aeda51912fce405e8b8ad
Your Name <you@example.com>
2021-04-06 23:02:08 +0530

    19

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,12 @@ from .forms import OrderForm,CreateUserForm
 from django.forms import inlineformset_factory
 from .filters import *
 from django.contrib.auth import authenticate,login,logout
-
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 # Create your views here.
 from django.http import  HttpResponse
+@login_required(login_url='login')
 def home(request):
     orders=Order.objects.all()
     customers=Customer.objects.all()
@@ -25,9 +26,13 @@ def home(request):
 
 
     return render(request,'accounts/dashboard.html',context)
+@login_required(login_url='login')
+
 def product(request):
     products=Product.objects.all()
     return render(request, 'accounts/product.html',{'products':products})
+@login_required(login_url='login')
+
 def customer(request,pk_test):
 
     customer=Customer.objects.get(id=pk_test)
@@ -38,6 +43,8 @@ def customer(request,pk_test):
 
     context={'customer':customer,'orders':orders,'order_count':order_count,'myfilter':myfilter}
     return render(request, 'accounts/customer.html',context)
+@login_required(login_url='login')
+
 def createOrder(request,pk):
     OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=5)
 
@@ -56,6 +63,8 @@ def createOrder(request,pk):
 
     context={'formset':formset}
     return render(request,'accounts/order_form.html',context)
+@login_required(login_url='login')
+
 def updateOrder(request,pk):
     order=Order.objects.get(id=pk)
     form=OrderForm(instance=order)
@@ -70,6 +79,8 @@ def updateOrder(request,pk):
 
     context={'form':form}
     return render(request,'accounts/order_form.html',context)
+@login_required(login_url='login')
+
 def deleteOrder(request,pk):
     order=Order.objects.get(id=pk)
     if request.method=='POST':
@@ -84,26 +95,39 @@ def deleteOrder(request,pk):
 
     return render(request,'accounts/delete.html',context)
 def loginpage(request):
-    if request.method=='POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user=authenticate(request,username=username,password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('home')
-        # context={}
-    return render(request,'accounts/login.html')
+    if request.user.is_authenticated:
+        return  redirect('home')
+    else:
+        if request.method=='POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username OR Password is Incorrect')
+                # return render(request,'accounts/login.html')
+            # context={}
+        return render(request,'accounts/login.html')
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return  redirect('home')
+    else:
 
-    if request.method =='POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')
-            messages.success(request,"Account was created for "+user)
-            return redirect('login')
-    context = {'form': form}
+        form = CreateUserForm()
+
+        if request.method =='POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,"Account was created for "+user)
+                return redirect('login')
+        context = {'form': form}
 
 
-    return render(request,'accounts/register.html',context)
+        return render(request,'accounts/register.html',context)
+def logoutuser(request):
+    logout(request)
+    return redirect('login')
