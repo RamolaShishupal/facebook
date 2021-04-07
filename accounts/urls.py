b6528fad4c358ab897e5b2198e2f5310806fa043
Your Name <you@example.com>
2021-04-07 15:31:35 +0530

    19

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,21 @@
+
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.home,name='home'),
-    path('login/',views.loginpage,name='login'),
-    path('register/',views.register, name='register'),
-    path('product/',views.product,name='product'),
-    path('customer/<str:pk_test>',views.customer,name='customer'),
-    path('create_order/<str:pk>',views.createOrder,name='create_order'),
-    path('update_order/<str:pk>',views.updateOrder,name='update_order'),
-    path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
-    path('logout/',views.logoutuser,name='logout'),
+
+
+urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutUser, name="logout"),
+
+    path('', views.home, name="home"),
+    path('user/', views.userPage, name="user-page"),
+    path('products/', views.products, name='product'),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+
 ]
