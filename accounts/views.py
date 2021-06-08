6f786a1e0dc276f36832eb4eeab40ca929fea8d4
Your Name <you@example.com>
2021-06-08 15:37:04 +0530

    24

--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,7 @@ def registerPage(request):
 
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user, name=user.username)
 
             messages.success(request, 'Account was created for ' + username)
 
