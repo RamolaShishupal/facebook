84257334255954239aba5b64f3bb1f70e896d862
Your Name <you@example.com>
2021-06-08 14:02:18 +0530

    24

--- accounts/views.py
+++ accounts/views.py
@@ -83,8 +83,12 @@ def home(request):
 @allowed_users(allowed_roles=['customer'])
 def userPage(request):
     orders=request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
     print('ORDERS :',orders)
-    context = {'orders':orders}
+    context = {'orders':orders,'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
     return render(request, 'accounts/user.html', context)
 
 
